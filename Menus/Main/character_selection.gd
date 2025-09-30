97116afe7ccacf5afbbfdac68fe82c49a801e407
Big Johnson <73569000+idkwidhere@users.noreply.github.com>
2025-09-30 11:14:45 -0400

    Done with character select
    Make pretty later

--- Menus/Main/character_selection.gd
+++ Menus/Main/character_selection.gd
@@ -11,6 +11,10 @@ var available_characters: Array[Character_Data] = [
 
 var chosen_character: Character_Data
 
+
+var scroll_speed : int = 300  # pixels per second
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -20,12 +24,21 @@ func _ready() -> void:
 		var temp_char_panel = CHARACTER_PANEL.instantiate()
 		temp_char_panel.character = character
 		%CharacterContainer.add_child(temp_char_panel)
-		
+
+
 func set_character(character):
 	GameLoader.selected_character = character
-	print(character)
-	var new_game = GAME.instantiate()
-	new_game.chosen_character = character
-	get_tree().root.add_child(new_game)
-	queue_free()
-	
+	print(character.character_name)
+
+
+func _on_start_button_pressed() -> void:
+	if GameLoader.selected_character:
+		var new_game = GAME.instantiate()
+		new_game.chosen_character = GameLoader.selected_character
+		get_tree().root.add_child(new_game)
+		queue_free()
+
+
+func _on_back_button_pressed() -> void:
+	hide()
+	get_tree().root.get_node("/root/MainMenu/MainScreen").show()
